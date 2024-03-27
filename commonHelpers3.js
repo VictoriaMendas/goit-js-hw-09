8b7f85a2cf22307d0dac9d8df005d0c9cd4bc0f9
VictoriaMendas <124498217+VictoriaMendas@users.noreply.github.com>
2024-03-27 16:40:42 +0000

    Deploying to gh-pages from @ VictoriaMendas/goit-js-hw-09@5e52fc8f036465fc43c06ba5ea4340bfbcacaa10 🚀

--- commonHelpers3.js
+++ commonHelpers3.js
@@ -1,2 +1,2 @@
-import"./assets/common-70025935.js";import"./assets/vendor-ff7dee2e.js";function s(o,e){return new Promise((i,n)=>{Math.random()>.3?i("hi"):n("goodbye")})}const r=document.querySelector(".form");r.addEventListener("submit",m);function m(o){console.log(o.target),setTimeout(()=>{s().then(({position:e,delay:t})=>{}).catch(({position:e,delay:t})=>{})},1e3)}
+import"./assets/common-70025935.js";import"./assets/vendor-5cb1b044.js";const o=document.querySelector(".form");o.addEventListener("submit",n);function n(t){t.preventDefault();const e=t.target;e.elements.delay.value,e.elements.step.value,e.elements.amount.value,console.log(e)}
 //# sourceMappingURL=commonHelpers3.js.map
